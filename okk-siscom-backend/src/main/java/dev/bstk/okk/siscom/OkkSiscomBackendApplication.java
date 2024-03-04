dea6bd2eeb65f925fe56ec47c99ba081d532f996
Bruno Luz <bsilva.se@gmail.com>
2024-03-04 18:16:23 -0300

    main - Adequando .gitignore

---
+++ okk-siscom-backend/src/main/java/dev/bstk/okk/siscom/OkkSiscomBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.bstk.okk.siscom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkkSiscomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkkSiscomBackendApplication.class, args);
+    }
+
+}
---
+++ okk-siscom-backend/src/test/java/dev/bstk/okk/siscom/OkkSiscomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bstk.okk.siscom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkkSiscomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
